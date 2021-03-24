06cfb6fbac00fa3253d9db08411ddbf6b2f5401e
Joanna Sakowska <joannadcs@gmail.com>
2021-03-24 11:26:16 +0100

    24/03/2021
    Adapted simple_adl for MC 2021 search on local clusters
    
    CHANGES INCLUDE:
    
    [1] Changes to the parallelisation of parallel_search.py by
    replacing csub with internal python packages;
    
    [2] Parallelisation of plot_hotspot.py with the addition of
    parallel_plot_hotspot.py;
    
    [3] A sample config.yaml and sample coordinate search file
    for an MC search (feel free to discard or use as reference);
    
    [4] README_MC2021 instructions on how to parallel search from either
    your machine or a computer cluster.

---
+++ README_MC2021.md
@@ -0,0 +1,81 @@
+# simple_adl
+An adaptation of the simple binner dwarf galaxy search algorithm for use on generic clusters for the MC 2021 dwarf search. 
+
+This version follows [`simple_adl`](https://github.com/sidneymau/simple_adl) which uses data pulled directly from the Astro Data Lab. Thanks Sidney and Will!
+
+Simple_adl originates from [`simple`](https://github.com/DarkEnergySurvey/simple).
+
+## Installation
+
+Clone this repository to the machine you wish to run the code from.
+
+Run the following command (consider adding to your `.bashrc`/`.zshrc`/etc.):
+```
+export PYTHONPATH=<path to simple_adl>:$PYTHONPATH
+```
+where `<path to simple_adl>` is the path to the inner simple_adl repository (ie, the full string should end with `*/simple_adl:$PYTHONPATH`).
+
+Create the conda environment with
+```
+conda env create -f conda.yaml
+```
+This will create a conda environment called `simple_adl` that includes all of the necessary software.
+
+## Use
+
+Activate the conda environment through `conda activate simple_adl`. Note that this needs to be done from a directory that does not have a `simple_adl` folder.
+
+Run `init_simple.py` to generate a default `config.yaml` configuration file.
+This can be edited as needed; in particular, you may want to change the `profile` entry.
+
+## New procedure for parallel searching
+
+Update the search_coordinates.csv file with the coordinates to be searched. 
+
+Open parallel_search.py. Enter the number of threads to run the search on in the 'n_threads' parameter. On linux, use $ lspcu to check how many threads per core your machine has (e.g., if there are 2 per core then a 7 core computer uses 14 threads). 
+
+NOTE: as the algorithm is quite cpu intense, it is recommended to try a few test runs with less threads (monitoring your cpu status) and find
+the best compromise.
+
+Adapt the parallel_search.py filepaths to your machine.
+
+Run on the command line:
+```
+python parallel_search.py 
+```
+Monitor the log_dir directory for output log information. 
+
+NOTE: if any of the log files are empty, the search failed (most likely due to an interuption of the program). Run it again.
+
+Once finished, run:
+```
+ python make_list.py
+```
+Inspect your candidate_list.csv.
+
+Open parallel_plot_hotspot.py. You can either read in candidate_list.csv or, after
+applying any cuts (such as 5 sigma), read in the new candidate_list file with a 
+modified name (such as candidate_list_5sigma.csv) and keep your old one for reference. 
+
+Set your number of threads and paths, run:
+```
+python parallel_plot_hotspot.py
+```
+
+And voila! Happy searching! 
+
+## Example
+
+To search for [DELVE 2](https://arxiv.org/abs/2009.08550), run the following:
+```
+python search.py --ra 28.77 --dec -68.25 
+```
+or
+```
+python search.py --nside 32 --ipix 11812
+```
+
+To plot DELVE 2, run
+```
+python plot_hotspot.py --ra 28.77 --dec -68.25 --mod 19.26
+```
---
+++ simple_adl/parallel_plot_hotspot.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+"""
+Python parallelisation to plot results from the candidate list 
+in result_dir
+Adapted for general clusters / personal machine
+"""
+__author__ = "Joanna Sakowska"
+
+import yaml
+import os
+import glob
+import subprocess
+
+import simple_adl.search
+
+import csv
+import pandas as pd
+
+import multiprocessing
+from multiprocessing import Pool
+
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~~~~~~~~~~~~~~~~~~~ DELVE-MC DWARF SEARCH 2021  ~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#
+
+# Enter number of threads to use 
+
+# Use $ lspcu to check how many threads per core
+# e.g. Joanna's has 2 per core, so max 14 threads = 7 cores
+
+n_threads = 14
+
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Sub-routines ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#
+
+def submit_plot_job(ra, dec, mod):
+    
+    # Output
+    outfile = 'candidate_{:0.2f}_{:0.2f}.png'.format(ra, dec)
+    
+    # Commands
+    command = 'python {}/plot_hotspot.py --ra {:0.2f} --dec {:0.2f} --mod {:0.2f} --outfile {}'.format(os.path.dirname(simple_adl.search.__file__), ra, dec, mod, outfile)
+    print(command)
+    print('Preparing plotting jobs...')
+    subprocess.call(command.split(' '), shell=False)
+
+
+
+    return
+
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Main search ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#
+
+if __name__ == '__main__':
+
+    # Reading config
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config',type=str,required=False,default='config.yaml',
+                        help='config file [.yaml]')
+    args = parser.parse_args()
+
+    with open(args.config, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+
+
+    # Reading in candidates
+    # JDS: Temporarily setting result directory
+    # JDS: Need to formalise this to use the config
+    results_dir = '/home/js01093/dwarf/simple_adl/simple_adl/results_dir'
+    plots_dir = '/home/js01093/dwarf/simple_adl/simple_adl/plot_dir/'
+
+
+    # Reading in candidates from folder
+    #candidates = pd.read_csv('{}/{}.csv'.format(results_dir, 'out'), delimiter=',', header=None)
+
+    # To read in candidates after make_list.py
+    #candidates = pd.read_csv('{}.csv'.format('candidate_list'), delimiter=',', header=None)
+
+    # To read in candidates AFTER 5 sigma cut
+    candidates = pd.read_csv('{}.csv'.format('candidate_list_5sig'), delimiter=',', header=None)
+
+    ra_candidate_all, dec_candidate_all, mod_candidate_all = [], [], []
+
+    ra_candidate_all.extend(candidates.iloc[:,1])
+    dec_candidate_all.extend(candidates.iloc[:,2])
+    mod_candidate_all.extend(candidates.iloc[:,3])
+
+    ra_candidate, dec_candidate, mod_candidate = [], [], []
+
+    # JDS: Method to skip overwriting plots in plot_dir
+    for ra, dec, mod in zip(ra_candidate_all, dec_candidate_all, mod_candidate_all):
+        if os.path.exists(os.path.join(plots_dir, 'candidate_{:0.2f}_{:0.2f}.png'.format(ra, dec))):
+            print('EXISTS candidate_{:0.2f}_{:0.2f}.png'.format(ra, dec))
+        else:
+            ra_candidate.append(ra)
+            dec_candidate.append(dec)
+            mod_candidate.append(mod)
+
+    # Clean memory
+    ra_candidate_all, dec_candidate_all, mod_candidate_all = [], [], []
+
+    print('Ready to plot candidates!')
+
+    # Zipping arguments to feed into command
+    plot_arguments = [*zip(ra_candidate,dec_candidate,mod_candidate)]
+
+    with Pool(n_threads) as p:
+        p.starmap(submit_plot_job, plot_arguments)
+        
+
